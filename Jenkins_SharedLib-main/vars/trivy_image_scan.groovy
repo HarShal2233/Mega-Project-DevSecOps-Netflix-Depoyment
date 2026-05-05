4d24c137a2094bc9f6f67d05727894697b605314
Harshal <harshalhajaresangli@gmail.com>
2026-05-05 15:30:49 +0000

    updated shared library of TRIVY IMAGE SCAN

--- Jenkins_SharedLib-main/vars/trivy_image_scan.groovy
+++ Jenkins_SharedLib-main/vars/trivy_image_scan.groovy
@@ -1,7 +1,16 @@
 def call(String imageName) {
     sh """
-    trivy image --severity HIGH,CRITICAL \
-    --format table \
-    ${imageName} > trivy-image-report.txt
+    echo "=============================="
+    echo " Trivy Image Scan Started"
+    echo " Image: ${imageName}"
+    echo "=============================="
+
+    trivy image \
+      --severity HIGH,CRITICAL \
+      --exit-code 1 \
+      --format table \
+      ${imageName} | tee trivy-image-report.txt
+
+    echo "Scan Completed"
     """
 }
