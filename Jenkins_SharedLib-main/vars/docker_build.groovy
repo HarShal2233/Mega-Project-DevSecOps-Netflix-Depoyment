ddaa1d8eb568195ad131260301877549eb634623
Harshal <harshalhajaresangli@gmail.com>
2026-05-05 17:10:35 +0000

    updated shared library of added api key in docker build

--- Jenkins_SharedLib-main/vars/docker_build.groovy
+++ Jenkins_SharedLib-main/vars/docker_build.groovy
@@ -1,4 +1,9 @@
-// Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(String imageName, String tag, String dockerHubUser) {
+    withCredentials([string(credentialsId: 'tmdb-api-key', variable: 'TMDB_KEY')]) {
+        sh """
+            docker build \
+            --build-arg TMDB_V3_API_KEY=$TMDB_KEY \
+            -t ${dockerHubUser}/${imageName}:${tag} .
+        """
+    }
 }
