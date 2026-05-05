a646905b7a16fc31105f15fdaa7a04cc94c914b9
Harshal <harshalhajaresangli@gmail.com>
2026-05-05 17:54:22 +0000

    secrets changed

--- Jenkins_SharedLib-main/vars/docker_build.groovy
+++ Jenkins_SharedLib-main/vars/docker_build.groovy
@@ -2,7 +2,7 @@ def call(String imageName, String tag, String dockerHubUser) {
     withCredentials([string(credentialsId: 'tmdb-api-key', variable: 'TMDB_KEY')]) {
         sh """
             docker build \
-            --build-arg TMDB_V3_API_KEY=$TMDB_KEY \
+            --build-arg VITE_TMDB_API_KEY=$TMDB_KEY \
             -t ${dockerHubUser}/${imageName}:${tag} .
         """
     }
